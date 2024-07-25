130903858afc43042bb79b4262175412142bee5a
Horst Dumcke <hdumcke@marlysys.com>
2024-07-25 14:42:02 +0200

    first checked in

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rpi-kids
+
+Install kids oriented software on a Raspberry Pi (5)
+
+## Installation
+
+Clone this repo and run
+
+```
+./build.sh
+```
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+sudo apt dist-upgrade -y
+sudo apt install -y gcompris klettres tuxpaint tuxmath tuxtype
+sudo apt install -y scratch3
+sudo apt install -y arduino
+sudo apt install -y git openssh-server python-is-python3 python3-venv curl python3-pip
