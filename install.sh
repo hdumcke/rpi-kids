401c7da9d28852d92b6c413bf1b3df4c554a5ad3
Horst Dumcke <hdumcke@marlysys.com>
2024-07-25 17:55:39 +0200

    update package name gcompris-qt

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -e
 
 sudo apt update
 sudo apt dist-upgrade -y
-sudo apt install -y gcompris klettres tuxpaint tuxmath tuxtype
+sudo apt install -y gcompris-qt klettres tuxpaint tuxmath tuxtype
 sudo apt install -y scratch3
 sudo apt install -y arduino
 sudo apt install -y git openssh-server python-is-python3 python3-venv curl python3-pip
+sudo reboot
